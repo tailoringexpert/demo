849daf8363ad2754402bae4d3a9874fc853d3a5c
Michael Bädorf <tailoringexpert@m.familiebaedorf.de>
2026-03-16 08:04:56 +0000

    adaptions for devcontainer

---
+++ tailoringexpert-demo-integrationtest/src/main/java/eu/tailoringexpert/DemoApp.java
@@ -0,0 +1,9 @@
+package eu.tailoringexpert;
+
+import org.springframework.boot.SpringApplication;
+
+public class DemoApp {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
